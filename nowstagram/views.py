08253afeedd8b129864ca8cb593b20f9ab5b806e
bluesli <1807103336@qq.com>
2019-02-13 00:21:13 +0800

    add user search and user setting but not finish

--- nowstagram/views.py
+++ nowstagram/views.py
@@ -336,13 +336,66 @@ def follow_friend(current_user_id,follow_user_id):
 @app.route('/settings/')
 @login_required
 def settings():
-    return render_template('user_settings.html')
+    # request.args
+    # request.form
+    if request.method == 'post':
+        username = request.values.get('username').strip()
+        password = request.values.get('password').strip()
+
+        # if username == '' or password == '':
+        #     return redirect_with_msg('/alter/', u'用户名或密码不能为空', 'reglogin')
+        #
+        user = User.query.filter_by(username=username).first()
+        print(user.username)
+        # if user == None:
+        #     return redirect_with_msg('/alter/', u'用户名不存在', 'reglogin')
+
+        # 更多判断
+
+        salt = '.'.join(random.sample('01234567890abcdefghigABCDEFGHI', 10))
+        m = hashlib.md5()
+        m.update(password + salt)
+        password = m.hexdigest()
+        user.password = password
+        user.salt = salt
+        db.session.commit()
+        return redirect('/regloginpage/')
+
+    else:
+        return render_template('user_settings.html')
 
 
 # 搜索
-@app.route('/search/')
+
+@app.route('/search/',methods={'post', 'get'})
 @login_required
 def search():
-    pass
+    # request.args
+    # request.form
+    if request.method == 'post':
+        username = request.values.get('username').strip()
+        password = request.values.get('password').strip()
+
+        # if username == '' or password == '':
+        #     return redirect_with_msg('/alter/', u'用户名或密码不能为空', 'reglogin')
+        #
+        user = User.query.filter_by(username=username).first()
+        print(user.username)
+        # if user == None:
+        #     return redirect_with_msg('/alter/', u'用户名不存在', 'reglogin')
+
+        # 更多判断
+
+        salt = '.'.join(random.sample('01234567890abcdefghigABCDEFGHI', 10))
+        m = hashlib.md5()
+        m.update(password + salt)
+        password = m.hexdigest()
+        user.password = password
+        user.salt = salt
+        db.session.commit()
+        return 'yes'
+
+    else:
+        return render_template('search_input.html')
 
 
